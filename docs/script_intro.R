ff33b653e85a1bee9882f27731553be6eae82fc2
Aldo Solari <solari.aldo@gmail.com>
2024-10-28 10:08:09 +0100

    update

--- docs/script_intro.R
+++ docs/script_intro.R
@@ -11,6 +11,7 @@ dim(titanic)
 # la "testa" della tabella
 head(titanic) 
 
+
 # variabile sopravvissuti
 sopravvissuti = titanic$survived
 # frequenze assolute
@@ -21,10 +22,12 @@ table(sopravvissuti)/length(sopravvissuti)
 tasso_sopravvivenza = (table(sopravvissuti)/length(sopravvissuti))[2]
 tasso_sopravvivenza
 
+
 # variabile classe
 classe = titanic$class
 table(classe)
 table(classe)/length(classe)
+
 
 # tabella di contingenza
 table(classe, sopravvissuti)
@@ -55,30 +58,49 @@ draws <- read_delim("https://www.stat.uchicago.edu/~stigler/draws.txt",
                     delim = "\t", escape_double = FALSE, 
                     col_names = FALSE, trim_ws = TRUE, 
                     show_col_types = FALSE)
+
+
+
+# la "testa" della tabella
 head(draws)
 
-draws[c(3749,3769),]
+# quanti numeri?
+prod(dim(draws))
 
+# tutti i numeri insieme
 numeri = unlist(draws)
 
+# frequenza osservata
 table(numeri)
 
+# frequenza attesa
 frequenza_attesa = length(numeri)/90
+frequenza_attesa
 
+# diagramma a bastoncini
 plot(table(numeri), 
      main = "Frequenza dei numeri estratti (1758-1834)",
      xlab = "Numero", ylab="Frequenza")
 abline(h=frequenza_attesa)
 
+# una strana coincidenza?
+draws[c(3749,3769),]
+
+# estrazioni ordinate
 draws_ordered = t(apply(draws,1,sort))
 head(draws_ordered)
 
+# quale elemento della tabella
 which(table(apply(draws_ordered, 1, function(x) paste(x,collapse=" "))) >= 2)
 
-N = nrow(draws)
-c = choose(90,5)
 
-# probability of at least one match
+# Supponi che N palline siano lasciate cadere a caso in c categorie.
+# Probabilità di nessuna coincidenza (al massimo una pallina) in ciascuna delle categorie?
+
+N = nrow(draws)  # numero di palline   (persone nella stanza)
+c = choose(90,5) # numero di categorie (giorni)
+
+# Probabilità di almeno una coincidenza
 pbirthday(n = N, classes=c)
-# approximation
+# Approssimazione
 1 - exp(-(N^2)/(2*c))
